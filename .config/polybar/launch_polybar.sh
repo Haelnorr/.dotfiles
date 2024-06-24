974d697b0dcaf46ce720f38c562e7e95830c7da8
Haelnorr <lockedonohoe@gmail.com>
2024-06-24 17:49:33 +1000

    fixed issues with polybar and i3 config

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,7 +1,7 @@
-if type "xrand"; then
+if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload toph &
+        MONITOR=$m polybar --reload haelnorr &
     done
 else
-    polybar --reload toph &
+    polybar --reload haelnorr &
 fi
